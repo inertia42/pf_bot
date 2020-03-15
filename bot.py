f64032756c4d4cb53594e9523d1315a6caa30d48
inertia <inertialiu@gmail.com>
2020-03-15 19:05:16 +0800

    ci: add a new rule to .gitignore and load the builtin plugins

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import config
 
 if __name__ == '__main__':
     nonebot.init(config)
+    nonebot.load_builtin_plugins()
     nonebot.load_plugins(
             path.join(path.dirname(__file__), 'plugins'),
         'plugins'
